307bbd2ab22e79236879dd6b7c2603f685ea3137
Gabriel Santos <gabr1el_santos@hotmail.com>
2020-02-17 09:53:29 -0300

    Created the basics modeling and testing for Partners

--- src/tests/utils/chance.ts
+++ src/tests/utils/chance.ts
@@ -1,12 +1,17 @@
-import Chance from 'chance';
+import Chance from "chance";
+import { Types } from "mongoose";
 
 class ExtendedChance extends Chance {
-  public int() {
+  public int(): number {
     return super.integer({ min: -2147483648, max: 2147483647 });
   }
 
-  public identity() {
+  public identity(): number {
     return super.integer({ min: 1, max: 214748 });
+  }
+
+  public objectId(): any {
+    return Types.ObjectId();
   }
 }
 
